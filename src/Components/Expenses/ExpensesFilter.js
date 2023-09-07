6af918bede4fa754d75f5b610fa26fcf2a3f20d2
Muaz Ashraf <muazashrafnet@gmail.com>
2023-09-07 14:50:21 +0500

    fix dates in chart

--- src/Components/Expenses/ExpensesFilter.js
+++ src/Components/Expenses/ExpensesFilter.js
@@ -11,11 +11,10 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select onChange={dropdownChangeHandler} value={props.selected}>
-          <option value="2022">2023</option>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+    { ["2018","2019","2020","2021","2022","2023"].map((year)=> <option key={year} value={year}>{year}</option>)}  
+      
+   
+    
         </select>
       </div>
     </div>
